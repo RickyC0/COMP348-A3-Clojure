38a53c5981f1173e040cb63e9620b58145d4df20
Ricardo Raji Chahine <ricardochahine0@gmail.com>
2025-04-08 22:18:11 -0400

    Implemented Option 1

--- menu.clj
+++ menu.clj
@@ -1,8 +1,7 @@
-(ns a3
+(ns menu
   (:require [clojure.string :as str])
   (:require [clojure.java.io :as io])
-  ; this is where you would also include/require the compress module
-  (:require [utils])  
+  ; this is where you would also include/require the compress module 
 )
   
 ; Display the menu and ask the user for the option
@@ -11,21 +10,35 @@
   (println "\n\n*** Grade Processing Menu ***")
   (println     "-----------------------------\n")
   (println "1. List Names")
-  (println "2. Process Something!")
-  (println "3. Another option")
-  (println "4. Run Test Code")
-  (println "5. ...")
+  (println "2. Display Student Record by Id")
+  (println "3. Display Student Record by Lastname")
+  (println "4. Display Component")
+  (println "5. Display Total")
   (println "6. Exit")
   (do 
-    (print "\nEnter an option? ") 
+    (print "\nEnter an option: ") 
     (flush) 
     (read-line)))
 
 
-; Replace the println expression with your own code (i.e. calling another function(s))
 (defn option1
-  [] ;parm(s) can be provided here, if needed
-  (println "this is a place holder for option 1"))
+  [students] ; 'students' here is a plain collection (vector) of student maps
+  (println "****************************************")
+  (println "*           STUDENT NAMES              *")
+  (println "****************************************")
+  ;; Print a header row with column names.
+  (println (format "%-10s | %-15s | %-15s" "ID" "First Name" "Last Name"))
+  (println "----------------------------------------------")
+  ;; For each student, print a formatted row.
+  (doseq [student students]
+    (println (format "%-10s | %-15s | %-15s"
+                     (:id student)
+                     (:first-name student)
+                     (:last-name student))))
+  (println "----------------------------------------------")
+  (println (format "Total Students: %d" (count students)))
+  (println "****************************************\n"))
+
     
     
 ; Replace the println expression with your own code
@@ -45,20 +58,24 @@
 (defn option4
   [] ;parm(s) can be provided here, if needed
   (print "\nRunning the test code")
-  (utils/run-test-code))
+)
+  
 
 
 
 ; If the menu selection is valid, call the relevant function to 
 ; process the selection
 (defn processOption
-  [option] ; other parm(s) can be provided here, if needed
+  [option,students] ; other parm(s) can be provided here, if needed
   (if( = option "1")
-     (option1)
+     (option1 students)
+    
      (if( = option "2")
         (option2)
+       
         (if( = option "3")
            (option3)  ; other args(s) can be passed here, if needed
+          
            (if( = option "4")
               (option4)   ; other args(s) can be passed here, if needed
               (println "Invalid Option, please try again"))))))
@@ -67,13 +84,14 @@
 ; Display the menu and get a menu item selection. Process the
 ; selection and then loop again to get the next menu selection
 (defn menu
-  [] ; parm(s) can be provided here, if needed
+  [students] ; parm(s) can be provided here, if needed
   (let [option (str/trim (showMenu))]
     (if (= option "6")
-      (println "\nGood Bye\n")
+      (println "\nGood Bye\n") 
+
       (do 
-         (processOption option)
-         (recur )))))   ; other args(s) can be passed here, if needed
+         (processOption option, students)
+         (recur students)))))   ; other args(s) can be passed here, if needed
 
 ; ------------------------------
 ; Run the program. You might want to prepare the data required for the mapping operations
@@ -83,4 +101,3 @@
 ; In general you need to read, process, and display the file content (grades.txt) 
 ; You may use Java's File class to check for existence. 
 
-(menu) ; other args(s) can be passed here, if needed
